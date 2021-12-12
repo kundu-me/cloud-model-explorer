ac6d460593a46f67278e943be689c9a985664322
Nirmallya Kundu <nkundu@mathworks.com>
2021-12-12 09:01:11 -0500

    initial commit

--- zc/db/dbAccess.py
+++ zc/db/dbAccess.py
@@ -1,11 +1,30 @@
-import connetionMgr
+import connectionMgr
 
-def getModel(name)
-    conn = connetionMgr.getConnection()
+def getModel(name):
+    conn = connectionMgr.getConnection()
     db = conn.systemcomposer_database
     systemcomposer_models = db['systemcomposer_models']
 
     #Find a document
     query = {'name': name}
     model = systemcomposer_models.find_one(query)
+    print(model)
     return model
+
+def getModels():
+    conn = connectionMgr.getConnection()
+    db = conn.systemcomposer_database
+    systemcomposer_models = db['systemcomposer_models']
+
+    #Get all document
+    cursor = systemcomposer_models.find({})
+    for document in cursor:
+          print(document)
+
+def setModel(model):
+    conn = connectionMgr.getConnection()
+    db = conn.systemcomposer_database
+    systemcomposer_models = db['systemcomposer_models']
+
+    #Insert a document
+    model = systemcomposer_models.insert_one(model)
